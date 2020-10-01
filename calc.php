7c8e2ce05bccb01f91ff929ab604bfe3161526c1
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-01 13:35:13 -0600

    Adds calculation logic (switch statement);

--- calc.php
+++ calc.php
@@ -1,11 +1,68 @@
 <?php 
     $GLOBALS['pageTitle'] = 'PHP Calculator';
     include './templates/header.php';
+    echo '<pre>';
+    var_dump( $_GET );
+    var_dump( $_POST );
+    echo '</pre>';
+
+    $result = FALSE;
+    if ( !empty( $_GET ) )
+    {
+        switch ( $_GET['op'] )
+        {
+            case 'addition':
+                $result = $_GET['value1'] + $_GET['value2'];
+                break;
+            case 'subtraction':
+                $result = $_GET['value1'] - $_GET['value2'];
+                break;
+            case 'multiplication':
+                $result = $_GET['value1'] * $_GET['value2'];
+                break;
+            case 'division':
+                $result = $_GET['value1'] / $_GET['value2'];
+                break;
+            default:
+                break;
+        }
+    }
 ?>
 
 <p>
     Welcome to our Calculator page!
 </p>
+
+<form method="GET" action="calc.php">
+    <label for="num1">
+        Enter first operand:
+        <input 
+            id="num1" 
+            name="value1"
+            type="number"
+            value="">
+    </label>
+      
+    <label for="operator">
+        Select an operator:
+        <select id="operator" name="op">
+            <option value="addition">+</option>
+            <option value="subtraction">-</option>
+            <option value="multiplication">&times;</option>
+            <option value="division">&divide;</option>
+        </select>
+    </label>
+  
+    <label for="num2">
+        Enter second operand:
+        <input 
+            id="num2" 
+            name="value2"
+            type="number"
+            value="">
+    </label>
+    <input type="submit" value="Calculate!">
+</form>
 
 <?php 
     include './templates/footer.php';
--- index.php
+++ index.php
@@ -1,46 +1,11 @@
 <?php 
     $GLOBALS['pageTitle'] = 'Home';
     include './templates/header.php';
-    echo '<pre>';
-    var_dump( $_GET );
-    var_dump( $_POST );
-    echo '</pre>';
 ?>
 
 <p>
     Welcome to our <?php echo $GLOBALS['pageTitle'] ?> page!
 </p>
-
-<form method="GET" action="calc.php">
-    <label for="num1">
-        Enter first operand:
-        <input 
-            id="num1" 
-            name="value1"
-            type="number"
-            value="">
-    </label>
-      
-    <label for="operator">
-        Select an operator:
-        <select id="operator" name="op">
-            <option value="addition">+</option>
-            <option value="subtraction">-</option>
-            <option value="multiplication">&times;</option>
-            <option value="division">&divide;</option>
-        </select>
-    </label>
-  
-    <label for="num2">
-        Enter second operand:
-        <input 
-            id="num2" 
-            name="value2"
-            type="number"
-            value="">
-    </label>
-    <input type="submit" value="Calculate!">
-</form>
 
 <?php 
     include './templates/footer.php';
