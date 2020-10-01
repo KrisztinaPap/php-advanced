414c17949d0175a84bc18bd15a32540048c05ff3
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-01 11:12:39 -0600

    Adds paragraph to index; Organizes templates on index;

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+    include './templates/header.php';
+?>
+
+<p>
+    Welcome to our PHP homepage!
+</p>
+
+<?php 
+    include './templates/footer.php';
+?>
--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,7 @@
     <p>
         &copy;
         <?php echo date( 'Y' ); ?>
+        TECHCareers
     </p>
 </footer>
 </body>
