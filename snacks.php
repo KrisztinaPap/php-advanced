46e86ce8e05297b370e03f6afbd5a04d59046238
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-02 13:36:07 -0600

    Adds json data;

--- snacks.php
+++ snacks.php
@@ -1,6 +1,19 @@
 <?php 
     $GLOBALS['pageTitle'] = 'Snacks Object Practice';
+    require_once './includes/Snack.Class.php';
     include './templates/header.php'; 
+
+    $snacksFileString = file_get_contents( './data/snacks.json ');
+
+    if ( $snacksFileString )
+    {
+        $snacksArray = json_decode( $snacksFileString );
+        
+        if ( $snacksArray )
+        {
+            var_dump( $snacksArray );
+        }
+    }
 ?>
 
 
