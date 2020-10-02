b46b2e8230695146e619dd7989c37936d3549a28
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-02 13:56:08 -0600

    Adds display for snacks;

--- snacks.php
+++ snacks.php
@@ -3,19 +3,31 @@
     require_once './includes/Snack.Class.php';
     include './templates/header.php'; 
 
+    $snacks = [];
+
     $snacksFileString = file_get_contents( './data/snacks.json ');
 
     if ( $snacksFileString )
     {
         $snacksArray = json_decode( $snacksFileString );
-        
+
         if ( $snacksArray )
         {
-            var_dump( $snacksArray );
+            foreach ( $snacksArray as $snack )
+            {
+                $snacks[] = new Snack( ...$snack );
+            }
+            // var_dump( $snacks );
         }
     }
 ?>
 
+<?php if ( !empty( $snacks ) ) : ?>
+    <h2>Our Snacks:</h2>
+    <?php foreach ( $snacks as $snack ) $snack->output(); ?>
+<?php else : ?>
+    <p>Sorry, no snacks found!</p>
+<?php endif; ?>
 
 
 <?php include './templates/footer.php'; ?>
