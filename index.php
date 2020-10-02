55e088124c595ea94cc3f42bdd1ba9592b15aaa4
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-02 10:30:15 -0600

    Adds calculation history on home page;

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+    session_start();
     $GLOBALS['pageTitle'] = 'Home';
     include './templates/header.php';
 ?>
@@ -6,6 +7,17 @@
 <p>
     Welcome to our <?php echo $GLOBALS['pageTitle'] ?> page!
 </p>
+
+<?php if( isset( $_SESSION['calcHistory'] ) ) : ?>
+    <h2>Calculator History (from current session only)</h2>
+    <ul>
+        <?php foreach ( $_SESSION['calcHistory'] as $calculation ) : ?>
+            <li>
+                <?php echo $calculation; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 <?php 
     include './templates/footer.php';
