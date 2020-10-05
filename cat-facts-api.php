0b090a52aca022a23a464bd52daf391b119871fe
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-05 11:06:55 -0600

    Displays random cat fact on HTML;

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,9 +1,20 @@
 <?php 
     session_start();
-    $GLOBALS['pageTitle'] = 'Home';
+    $GLOBALS['pageTitle'] = 'Cat Facts - (External API Test)';
     include './templates/header.php';
-?>
 
-<?php 
+    // @link https://alexwohlbruck.github.io/cat-facts/docs/endpoints/facts.html
+    $dailyCatFactResponse = file_get_contents( 'http://cat-fact.herokuapp.com/facts/random' );
+
+    if ( $dailyCatFactResponse )
+    {
+        $dailyCatFact = json_decode( $dailyCatFactResponse );
+
+        ?>
+            <h2>Today's cat fact:</h2>
+            <p><?php echo $dailyCatFact->text; ?></p>
+        <?php
+    }
+
     include './templates/footer.php';
 ?>
