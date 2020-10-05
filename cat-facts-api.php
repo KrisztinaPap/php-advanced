6b999628331388be9b45390265d6a1cd71a6800b
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-05 11:42:23 -0600

    Adds form to request facts (number of facts and type of animal);

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -4,7 +4,7 @@
     include './templates/header.php';
 
     // @link https://alexwohlbruck.github.io/cat-facts/docs/endpoints/facts.html
-    $dailyCatFactResponse = file_get_contents( 'http://cat-fact.herokuapp.com/facts/random' );
+    $dailyCatFactResponse = file_get_contents( 'https://cat-fact.herokuapp.com/facts/random' );
 
     if ( $dailyCatFactResponse )
     {
@@ -16,5 +16,41 @@
         <?php
     }
 
+    ?>
+
+    <h2>Request Animal Facts</h2>
+    <form action="#" method="POST">
+        <label for="amount">Enter the Amount of Facts:
+        <input type="number" id="amount" name="amount"></label>
+        <label for="animal-type">Enter the Type of Animal:
+        <input type="text" id="animal-type" name="type"></label>
+        <input type="submit" value="Get Animal Facts!">
+    </form>    
+
+    <?php
+
+    if (isset( $_POST['amount']) && isset( $_POST['type']))
+    {
+        $factsListResponse = file_get_contents( "https://cat-fact.herokuapp.com/facts/random?amount={$_POST['amount']}&animal_type={$_POST['type']}");
+
+        if ( $factsListResponse )
+        {
+            $factsList = json_decode( $factsListResponse );
+            ?>
+                <h2>List of 
+                <?php echo ucfirst( $_POST['type']) ; ?>
+                Facts</h2>
+            <ol>
+                <?php foreach ( $factsList as $fact ) : ?>
+                    <li>
+                        <?php echo $fact->text; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ol>
+            <?php
+        }
+    }
+    
+    
+
     include './templates/footer.php';
-?>
