0a53f925d2b08725a0b62fa62799a00c3900efd3
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-06 10:25:01 -0600

    Adds search form;

--- js/react-example.js
+++ js/react-example.js
@@ -1,4 +1,30 @@
 const reactRoot = document.getElementById('react-root');
 
+const SearchForm = props => {
 
-ReactDOM.render( <p>Hello, world!</p>, reactRoot);
+    const [search, setSearch] = React.useState( '' );
+
+    return (
+        <React.Fragment>
+            <h2>Snack Search Form</h2>
+            <form>
+                <label htmlFor="search">
+                    Enter a Search Term:
+                    <input 
+                        type="search"
+                        id="search" 
+                        onChange={event => { setSearch( event.target.value ) } }
+                        value={search} />
+                </label>
+                <input 
+                    type="submit"
+                    value="Search Snacks" />
+            </form>
+            <h3>Snack Search Results</h3>
+            <ul></ul>
+        </React.Fragment>
+    )
+}
+
+
+ReactDOM.render( <SearchForm />, reactRoot );
